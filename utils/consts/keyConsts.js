c2e74519cd88fa6c4ae5ec0ecfa0c84abe77e086
Petr Mitin <petr09mitin@mail.ru>
2022-12-12 23:06:11 +0300

    Added production data and percentage

--- utils/consts/keyConsts.js
+++ utils/consts/keyConsts.js
@@ -1,35 +1,35 @@
 const keyRewards = {
     common: [{
         reward: 'Скидка 3%',
-        chance: 20
+        chance: 10
     },
     {
         reward: 'Бесплатная доставка',
-        chance: 20
+        chance: 5
     },
     {
         reward: 'Эпический ключ',
-        chance: 20
+        chance: 10
     }],
     epic: [{
         reward: 'Любой товар до 200 Юань (¥)',
-        chance: 20
+        chance: 3
     },
     {
         reward: 'Скидка 7%',
-        chance: 20
+        chance: 10
     },
     {
         reward: 'Легендарный ключ',
-        chance: 20
+        chance: 5
     }],
     legendary: [{
         reward: 'Скидка 10%',
-        chance: 20
+        chance: 10
     },
     {
         reward: 'Любой товар до 500 Юань (¥)',
-        chance: 20
+        chance: 3
     }]
 }
 
