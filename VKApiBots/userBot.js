a11fb0f0fba15412a037364d6b3131515dec1471
Petr Mitin <petr09mitin@mail.ru>
2022-12-12 23:28:51 +0300

    Fixed message/context

--- VKApiBots/userBot.js
+++ VKApiBots/userBot.js
@@ -6,9 +6,9 @@ const userBot = new HearManager()
 
 userBot.hear('пойз', async (context) => {
     if (context.isOutbox) return
-    keyService.addToUserKeys(context.senderId, 'common', 30)
-    keyService.addToUserKeys(context.senderId, 'epic', 30)
-    keyService.addToUserKeys(context.senderId, 'legendary', 30)
+    // keyService.addToUserKeys(context.senderId, 'common', 30)
+    // keyService.addToUserKeys(context.senderId, 'epic', 30)
+    // keyService.addToUserKeys(context.senderId, 'legendary', 30)
     return context.send(`Приветствуем! Чтобы использовать игрового бота, откройте клавиатуру (квадрат с 4 точками справа от смайликов)`, {
         keyboard: startKeyboard
     })
